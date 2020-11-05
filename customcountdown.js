462eccbd4dfba8c3af8027870d17f0677f560999
JI-76 <johnison76@hotmail.com>
2020-11-05 15:09:20 -0800

    Javascript Ddates

--- customcountdown.js
+++ customcountdown.js
@@ -1,1 +1,12 @@
 // customcountdown.js
+
+// Add const to handle HTML Elements on Input View 
+const inputContainer = document.getElementById('input-container');
+const countdownFrom = document.getElementById('countdownFrom');
+const dateEl = document.getElementById('date-picker');
+
+// Set Date Input Min with current date
+const today = new Date().toISOString().split('T')[0];
+//console.log(today);
+
+dateEl.setAttribute('min', today);
