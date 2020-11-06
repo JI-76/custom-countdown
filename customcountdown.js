91c5d4f913d7bf0329147581733d26e3ffc5a264
JI-76 <johnison76@hotmail.com>
2020-11-05 16:01:11 -0800

    Form Values

--- customcountdown.js
+++ customcountdown.js
@@ -5,8 +5,30 @@ const inputContainer = document.getElementById('input-container');
 const countdownFrom = document.getElementById('countdownFrom');
 const dateEl = document.getElementById('date-picker');
 
+// Add global variables
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with current date
 const today = new Date().toISOString().split('T')[0];
 //console.log(today);
 
 dateEl.setAttribute('min', today);
+
+// Gather Form Input values
+function updateCountdown(e) {
+    // prevent page from refreshing and clearing input values
+    e.preventDefault();
+
+    // capture Form Input values
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+
+    //console.log(e);
+    console.log(countdownTitle);
+    console.log(countdownDate);
+}
+
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
